8acffb57592571231804a2f772ea679e9a412e0e
rossi <rossi.kwak@gmail.com>
2016-09-28 20:56:01 +0900

    Parameters (C, sigma) for Dataset 3 |  25 /  25 | Nice work!

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,8 +23,26 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+values=[ 0.01 0.03 0.1 0.3 1 3 10 30 ];
+error = 1000000;
 
-
+for i=1:size(values,2)
+    for j=1:size(values,2)
+        model= svmTrain(X, y, values(i), @(x1, x2) gaussianKernel(x1,x2, values(j)));
+        predictions = svmPredict(model,Xval);
+        new_error = mean(double(predictions ~= yval));
+        
+        fprintf('C:%f sigma:%f new error:%f',values(i),values(j),new_error);
+        
+        if new_error < error
+            error = new_error;
+            C = values(i);
+            sigma = values(j);
+            fprintf(',optimal C:%f sigma:%f error:%f',C,sigma,error);
+        end
+        fprintf('\n');
+    end
+end
 
 
 
